1d83d551e29113df79b82653d7995081ac308f4a
rmelido329 <rmelido329@student.fuhsd.org>
2025-03-07 09:54:23 -0800

    Added Detector.java
    Combines all necessary filters into one

---
+++ src/Filters/Detector.java
@@ -0,0 +1,20 @@
+package Filters;
+
+import Interfaces.PixelFilter;
+import core.DImage;
+
+import java.awt.*;
+
+public class Detector implements PixelFilter {
+    @Override
+    public DImage processImage(DImage img) {
+        PixelFilter[] filterList = {
+                new ColorMask()
+        };
+        for (PixelFilter filter : filterList) {
+            img = filter.processImage(img);
+        }
+        return img;
+    }
+}
+
