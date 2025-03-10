4e8ff40d586ba7fb59dee71a8dc3f661f2136510
rmelido329 <rmelido329@student.fuhsd.org>
2025-03-10 12:34:22 -0700

    Updated ColorMask.java -- detects based on saturation of color values

--- src/Filters/ColorMask.java
+++ src/Filters/ColorMask.java
@@ -6,7 +6,7 @@ import core.DImage;
 import java.awt.*;
 
 public class ColorMask implements PixelFilter {
-    private float th;
+    private double th=0.05;
     private double threshold = 0.1;
 
     @Override
@@ -21,10 +21,10 @@ public class ColorMask implements PixelFilter {
             for (int c = 0; c < rr[r].length; c++) {
 
                 Color.RGBtoHSB(rr[r][c],gg[r][c],bb[r][c],hsv);
-                boolean hCheck = Math.abs(th-hsv[0])<=threshold;
-                boolean sCheck = 1-hsv[1]<=0.5;
-                boolean vCheck = 1-Math.abs(hsv[2]-10)<=0.7;
-                short val = (short)((hCheck&&sCheck&vCheck)?255:0);
+//                boolean hCheck = Math.abs(th-hsv[0])<=threshold || Math.abs(th-hsv[0]-360)<=threshold || Math.abs(th-hsv[0]+360) <= threshold;
+                boolean sCheck = hsv[1]>=0.5;
+//                boolean vCheck = 1-Math.abs(hsv[2]-10)<=0.7;
+                short val = (short)((sCheck)?255:0);
                 if (r==0 && c==0) System.out.println(hsv[2]);
                 rr[r][c] = val;
                 gg[r][c] = val;
