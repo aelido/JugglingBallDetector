aae8e0c58b1dd5a03e76a595d9e5f4a8690d8712
rmelido329 <rmelido329@student.fuhsd.org>
2025-03-14 10:24:01 -0700

    Changed values for target hue and saturations in Detector.java

--- src/Filters/Detector.java
+++ src/Filters/Detector.java
@@ -7,8 +7,8 @@ import java.awt.*;
 
 public class Detector implements PixelFilter {
     private double hueDist = 0.1;
-    private double[] targetHues = {0.9, 0.15, 0.3, 0.6};
-    private double[] targetSats = {0.7, 0.7, 0.4, 0.4};
+    private double[] targetHues = {0.9, 0.15, 0.4, 0.6};
+    private double[] targetSats = {0.7, 0.7, 0.1, 0.4};
     private short[][] targetHSVtoRGB= {
             {255,0,0}, {255,100,0}, {0,255,0}, {0,0,255}
     };
