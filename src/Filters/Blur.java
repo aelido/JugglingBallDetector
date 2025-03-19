8662b7ddc2cf6696f2b5734f9624b3328c0491d2
K9-2026 <jowmama951@gmail.com>
2025-03-19 09:54:23 -0700

    Finally Fixed Stupid Blur.java

--- src/Filters/Blur.java
+++ src/Filters/Blur.java
@@ -10,32 +10,53 @@ public class Blur implements PixelFilter {
 
     private int size;
     private double[][] kernel;
-    private double kernelWeight=0;
-
+    private double kernelWeight = 0;
 
     public Blur() {
-        String j = JOptionPane.showInputDialog("Would you like to change Kernel? ");
-        if(Objects.equals(j, "yes")){
-            String r = JOptionPane.showInputDialog("Enter Size of Kernel: ");
-            String l = JOptionPane.showInputDialog("Enter Kernel number: ");
-            size = Integer.parseInt(r);
-            int val = Integer.parseInt(l);
-            kernelSettings(val);
-        }else{
-            kernel = new double[5][5];
-        }
-
+        ask();
     }
-    public void kernelSettings(int val){
+
+    public void ask() {
+        String l = JOptionPane.showInputDialog("Would you like to change Kernel Settings?");
+        if (l.equals("yes")) {
+            kernelSettings();
+        } else {
+            AutoKernel();
+        }
+    }
+
+    public void kernelSettings() {
+        // System.out.println("This has ran!");
+        String r = JOptionPane.showInputDialog("Enter Size of Kernel: ");
+        String l = JOptionPane.showInputDialog("Enter Kernel number: ");
+        size = Integer.parseInt(r);
+        int val = Integer.parseInt(l);
         kernel = new double[size][size];
-        kernelWeight=0;
-        for (int y=0; y<kernel.length; y++) { for (int x=0; x<kernel[0].length; x++) kernel[y][x]=val;}
-        for (int y=0; y<kernel.length; y++) { for (int x=0; x<kernel[0].length; x++) kernelWeight+=kernel[y][x]; }
-        if (kernelWeight==0) kernelWeight=1;
+        kernelWeight = 0;
+        for (int y = 0; y < kernel.length; y++) {
+            for (int x = 0; x < kernel[0].length; x++) kernel[y][x] = val;
+        }
+        for (double[] doubles : kernel) {
+            for (int x = 0; x < kernel[0].length; x++) kernelWeight += doubles[x];
+        }
+        if (kernelWeight == 0) kernelWeight = 1;
+    }
+
+    public void AutoKernel() {
+        kernel = new double[5][5];
+        kernelWeight = 0;
+        for (int y = 0; y < kernel.length; y++) {
+            for (int x = 0; x < kernel[0].length; x++) kernel[y][x] = 1;
+        }
+        for (double[] doubles : kernel) {
+            for (int x = 0; x < kernel[0].length; x++) kernelWeight += doubles[x];
+        }
+        if (kernelWeight == 0) kernelWeight = 1;
     }
 
     @Override
     public DImage processImage(DImage img) {
+
         short[][] rr = img.getRedChannel();
         short[][] gg = img.getGreenChannel();
         short[][] bb = img.getBlueChannel();
@@ -45,26 +66,26 @@ public class Blur implements PixelFilter {
         short[][] gc = gg.clone();
         short[][] bc = bb.clone();
         //
-        for (int r = 0; r < rr.length-kernel.length; r++) {
-            for (int c = 0; c < rr[r].length-kernel[0].length; c++) {
-                rc[r+(size/2)][c+(size/2)]=kernelValue(rr,kernel,r,c);
-                gc[r+(size/2)][c+(size/2)]=kernelValue(gg,kernel,r,c);
-                bc[r+(size/2)][c+(size/2)]=kernelValue(bb,kernel,r,c);
+        for (int r = 0; r < rr.length - kernel.length; r++) {
+            for (int c = 0; c < rr[r].length - kernel[0].length; c++) {
+                rc[r + (size / 2)][c + (size / 2)] = kernelValue(rr, kernel, r, c);
+                gc[r + (size / 2)][c + (size / 2)] = kernelValue(gg, kernel, r, c);
+                bc[r + (size / 2)][c + (size / 2)] = kernelValue(bb, kernel, r, c);
             }
         }
 
-        img.setColorChannels(rc,gc,bc);
+        img.setColorChannels(rc, gc, bc);
         return img;
     }
 
     private short kernelValue(short[][] img, double[][] kernel, int r, int c) {
         double val = 0;
-        for (int y=0; y<kernel.length; y++) {
-            for (int x=0; x<kernel[0].length; x++) {
-                val += img[r+y][c+x] * kernel[y][x];
+        for (int y = 0; y < kernel.length; y++) {
+            for (int x = 0; x < kernel[0].length; x++) {
+                val += img[r + y][c + x] * kernel[y][x];
             }
         }
-        return (short)(val/kernelWeight);
+        return (short) (val / kernelWeight);
     }
 }
 
--- src/Filters/Detector.java
+++ src/Filters/Detector.java
@@ -6,14 +6,15 @@ import core.DImage;
 import java.awt.*;
 
 public class Detector implements PixelFilter {
+    PixelFilter[] filterList = {
+            new Blur(),
+            new ColorMask()
+    };
     @Override
     public DImage processImage(DImage img) {
-        PixelFilter[] filterList = {
-                new Blur(),
-                new ColorMask()
-        };
-        for (PixelFilter filter : filterList) {
-            img = filter.processImage(img);
+
+        for (PixelFilter filter:  filterList) {
+          img = filter.processImage(img);
         }
         return img;
     }
