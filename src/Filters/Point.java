63698c4fabbae2d3e68223cdaf72e87be9730986
rmelido329 <rmelido329@student.fuhsd.org>
2025-03-19 10:36:30 -0700

    Added dist() and toString() method

--- src/Filters/Point.java
+++ src/Filters/Point.java
@@ -2,7 +2,7 @@ package Filters;
 
 public class Point {
     private int x, y;
-    public Point(int y, int x) { this.y=y; this.x=x;}
+    public Point(int y, int x) { set(y,x); }
     public int getX() {
         return x;
     }
@@ -12,9 +12,19 @@ public class Point {
     public int[] get() {
         return new int[]{y,x};
     }
-    public float dist(Point p) {
+    public void set(Point p) {
+        set(p.getY(), p.getX());
+    }
+    public void set(int y, int x) {
+        this.y=y; this.x=x;
+    }
+
+    public int dist(Point p) {
         int px = p.getX(), py = p.getY();
-        return (float)Math.sqrt((px-x)*(px-x)+(py-y)*(py-y));
+        return dist(px,py);
+    }
+    public int dist(int py, int px) {
+        return (px-x)*(px-x)+(py-y)*(py-y);
     }
 
     @Override
